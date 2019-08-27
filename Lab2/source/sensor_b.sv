ee6f6b176972bbeec1b1be44f8f6fc3753cc0440
Shivam Duhan <sduhan@purdue.edu>
2019-08-27 06:11:03 -0400

    created skeleton file for behavorial style sensor erro detector code

---
+++ Lab2/source/sensor_b.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   sensor_b.sv
+// Created:     8/27/2019
+// Author:      Shivam Duhan
+// Lab Section: 337-03
+// Version:     1.0  Initial Design Entry
+// Description: Behavorial style Sensor Error Detector Code
