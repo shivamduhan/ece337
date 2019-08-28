8a540dafd4c9d9e0b0a763a14cb4b1983caea81d
Shivam Duhan <sduhan@purdue.edu>
2019-08-28 10:47:59 -0400

    Tested behavorial style sensor error detector design. 100% success.

--- Lab2/source/sensor_b.sv
+++ Lab2/source/sensor_b.sv
@@ -5,3 +5,14 @@
 // Lab Section: 337-03
 // Version:     1.0  Initial Design Entry
 // Description: Behavorial style Sensor Error Detector Code
+
+module sensor_b
+(
+ input wire [3:0] sensors,
+ output reg error
+ );
+   always_comb
+   begin
+      error = sensors[0] | ((sensors[3] | sensors[2]) & sensors[1]);
+   end   
+endmodule 
