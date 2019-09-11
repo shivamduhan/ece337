c6fe364558051f9f93d5a81cfd48915a0222d9db
Shivam Duhan <sduhan@purdue.edu>
2019-09-11 13:52:17 -0400

    synch high

---
+++ Lab4/source/sync_high.sv
@@ -0,0 +1,23 @@
+
+module sync_high
+  (
+   input reg clk,
+   input reg n_rst,
+   input reg async_in,
+   output reg sync_out
+   );   
+
+   reg 	       pass;
+   
+  always_ff @ (posedge clk, negedge n_rst)
+  begin
+     if (1'b0 == n_rst) begin
+	sync_out <= 1'b1;
+       	pass <= 1'b0;
+	      
+     end else begin
+	pass <= async_in;	
+	sync_out <= pass;
+     end 	 
+  end
+   endmodule
